da5afb8fe7ea8e0be00e05252516741bfcdbbed8
Godswill Edet <71720298+Godswilldev@users.noreply.github.com>
2021-04-14 13:39:19 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,24 @@
+// Importing the file system module
 const fs = require("fs");
 
-const url = "https://jsonplaceholder.typicode.com/posts";
-
+//Importing the axios module.
 const axios = require("axios");
 
+//JSON placeholder URL
+const url = "https://jsonplaceholder.typicode.com/posts";
+
+//Using axios to fetch the posts from the URL
 axios
   .get(url)
   .then((response) => {
+
+//Converting the response gotten from the URL to a JSON string
     const jsonData = JSON.stringify(response.data);
+
+//Writing the JSON data to a new file called in the results directory called "posts.json"
     fs.writeFile("result/posts.json", jsonData, (err) => {
+
+// Error/ success message
       err ? console.log(err) : console.log("Wrote file successfully");
     });
   })
